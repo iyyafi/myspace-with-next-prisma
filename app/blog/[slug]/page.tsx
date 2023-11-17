78151e15dede6cec58878dec21c38ad906dcde0d
Iyyafi Qolbi <iyyafi@gmail.com>
2023-11-17 08:54:43 +0700

    static generation

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,19 @@
+export const revalidate = 1200; // not necessary, just for ISR demonstration
+
 interface Post {
   title: string;
   content: string;
   slug: string;
+}
+
+export async function generateStaticParams() {
+  const posts: Post[] = await fetch("http://localhost:3000/api/content").then(
+    (res): Promise<Post[]> => res.json()
+  );
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
 }
 
 interface Props {
