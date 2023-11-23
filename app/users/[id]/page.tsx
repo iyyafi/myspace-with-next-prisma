1c183ac6d9d525760e9b391036e299122104bc8e
Iyyafi Qolbi <iyyafi@gmail.com>
2023-11-23 14:46:12 +0700

    add follow component

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,4 +1,4 @@
-// import FollowButton from "@/components/FollowButton/FollowButton";
+import FollowButton from "@/components/FollowButton/FollowButton";
 import { prisma } from "@/lib/prisma";
 import { Metadata } from "next";
 import Image from "next/image";
@@ -31,6 +31,8 @@ export default async function UserProfile({ params }: Props) {
 
       <h3>Bio</h3>
       <p>{bio}</p>
+
+      <FollowButton targetUserId={params.id} />
     </div>
   );
 }
